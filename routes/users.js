b116fec99af4f6a6d5a6e1cc1cadcf5cb134deb3
desai-pratik <pratikdesai2274@gmail.com>
2024-06-14 15:57:25 +0530

    change in user.js file.

--- routes/users.js
+++ routes/users.js
@@ -44,7 +44,7 @@ router.get('/', async (req, res) => {
     const userId = req.query.userId;
     const username = req.query.username;
     try {
-        const user = userId ? await User.findById(req.params.id):await User.findOne({username: username});
+        const user = userId ? await User.findById(userId) : await User.findOne({ username: username });
         const { password, updatedAt, ...other } = user._doc
         res.status(200).json(other)
     } catch (err) {
