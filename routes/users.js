1cfceadec454785b66f5a547df7488c95e048c43
desai-pratik <pratikdesai2274@gmail.com>
2024-06-17 22:08:58 +0530

    get user friends api add

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 
-
 // update user
 router.put('/:id', async (req, res) => {
     if (req.body.userId === req.params.id || req.body.isAdmin) {
@@ -91,6 +90,26 @@ router.put('/:id/unfollow', async (req, res) => {
         }
     } else {
         res.status(403).json("you can't unfollow yourself.")
+    }
+});
+
+// get user friends
+router.get('/friends/:userId', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.userId);
+        const friends = await Promise.all(
+            user.followings.map((friendId) => {
+                return User.findById(friendId);
+            })
+        );
+        let friendList = [];
+        friends.map((friend) => {
+            const { _id, username, profilePicture } = friend;
+            friendList.push({ _id, username, profilePicture })
+        })
+        res.status(200).json(friendList)
+    } catch (err) {
+        return res.status(500).json(err);
     }
 });
 
