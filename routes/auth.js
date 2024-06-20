ec30249b551ca0263a02c6768932fb5571a32a25
desai-pratik <pratikdesai2274@gmail.com>
2024-06-20 20:11:52 +0530

    some validation add in auth api

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,19 @@
 const router = require("express").Router()
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
-
+const { body, validationResult } = require('express-validator');
 
 // register
-router.post('/register', async (req, res) => {
+router.post('/register', [
+      body('username').isLength({ min: 3 }).withMessage('Username must be at least 3 characters long'),
+    body('email').isEmail().withMessage('Please enter a valid email address'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+], async (req, res) => {
+     const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
     try {
         const salt = await bcrypt.genSalt(10);
         const hasHedPassword = await bcrypt.hash(req.body.password, salt);
@@ -17,27 +26,32 @@ router.post('/register', async (req, res) => {
         res.status(200).json(user)
     } catch (err) {
         console.log(err);
-        res.status(500).json(err);
+         res.status(500).json({ error: 'Internal server error' });
     }
 });
 
 // login
-router.post('/login', async (req, res) => {
+router.post('/login', [
+    body('email').isEmail().withMessage('Please enter a valid email address'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
     try {
         const user = await User.findOne({ email: req.body.email });
         if (!user) {
-            return res.status(404).json('email not found.');
+            return res.status(404).json({ error: 'Email not found.' });
         }
-
         const validPassword = await bcrypt.compare(req.body.password, user.password);
         if (!validPassword) {
-            return res.status(400).json('Wrong password.');
+            return res.status(400).json({ error: 'Wrong password.' });
         }
-
         res.status(200).json(user);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Internal server error' });
     }
 });
 
