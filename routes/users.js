a79f04a324e9fc4e3c9b2397df762d4db1825678
desai-pratik <pratikdesai2274@gmail.com>
2024-06-20 10:05:10 +0530

    update user

--- routes/users.js
+++ routes/users.js
@@ -15,7 +15,7 @@ router.put('/:id', async (req, res) => {
         }
         try {
             const user = await User.findByIdAndUpdate(req.params.id, { $set: req.body });
-            res.status(200).json('Account has been updated.')
+            res.status(200).json(user)
         } catch (err) {
             return res.status(500).json(err);
         }
