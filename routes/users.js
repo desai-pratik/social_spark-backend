5a96d0bae9082b9fc560614e5e6a28d1347055b1
desai-pratik <pratikdesai2274@gmail.com>
2024-06-20 11:17:04 +0530

    new user added in update user

--- routes/users.js
+++ routes/users.js
@@ -14,8 +14,8 @@ router.put('/:id', async (req, res) => {
             }
         }
         try {
-            const user = await User.findByIdAndUpdate(req.params.id, { $set: req.body });
-            res.status(200).json(user)
+            const user = await User.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+            res.status(200).json(user);
         } catch (err) {
             return res.status(500).json(err);
         }
