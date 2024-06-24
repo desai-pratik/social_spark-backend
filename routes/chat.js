7d8675ce09ae356fa70f5bb6ad8ed6712d8d2116
desai-pratik <pratikdesai2274@gmail.com>
2024-06-24 15:30:00 +0530

    updated chat.js

--- routes/chat.js
+++ routes/chat.js
@@ -46,18 +46,30 @@ router.post("/", verifyToken, async (req, res) => {
 // get chat for all users
 router.get("/", verifyToken, async (req, res) => {
   try {
-    await Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+    // await Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+    //   .populate("users", "-password")
+    //   .populate("groupAdmin", "-password")
+    //   .populate("latestMessage")
+    //   .sort({ updatedAt: -1 })
+    //   .then(async (results) => {
+    //     results = await User.populate(results, {
+    //       path: "latestMessage.sender",
+    //       select: "username profilePicture email"
+    //     });
+    //     res.status(200).send(results);
+    //   });
+    
+    const chats = await Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
       .populate("users", "-password")
       .populate("groupAdmin", "-password")
       .populate("latestMessage")
-      .sort({ updatedAt: -1 })
-      .then(async (results) => {
-        results = await User.populate(results, {
-          path: "latestMessage.sender",
-          select: "username profilePicture email"
-        });
-        res.status(200).send(results);
-      });
+      .sort({ updatedAt: -1 });
+
+    const populatedChats = await User.populate(chats, {
+      path: "latestMessage.sender",
+      select: "username profilePicture email"
+    });
+    res.status(200).send(populatedChats);
 
   } catch (error) {
     res.status(400).json(error);
