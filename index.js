4012461fb94df729d8c91211219329d33b944f2b
desai-pratik <pratikdesai2274@gmail.com>
2024-07-21 19:01:55 +0530

    url change

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const server = app.listen(4000, () => {
 const io = require('socket.io')(server, {
     pingTimeout: 6000,  // this is for waiting second
     cors: {
-        origin: "https://socialspark01.netlify.app",  // this is solve cors error for chat. this is frontend link
+        origin: "http://localhost:3000",  // this is solve cors error for chat. this is frontend link
     },
 });
 
